3e483e2f1df0f7b3111d2807440070ec672ffd70
Erland Van Reet <erlandvr@gmail.com>
2018-08-07 18:01:00 +0200

    put content with links in contact-page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'start', to: 'pages#start', as: :start
   get 'show', to: 'pages#show', as: :show
+  get 'index', to: 'pages#index', as: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
